e62b58e28a2b2d3587ebf4f48099d25f19d00d41
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-06-01 22:44:19 +0300

    JS added

---
+++ index.js
@@ -0,0 +1,32 @@
+// Меняем активный класс у основной менюшки
+const mainItemsContainer = document.querySelector('.main-items')
+
+mainItemsContainer.addEventListener('click', event => {
+	if (event.target.classList.contains('main-items__button')) {
+		document.querySelectorAll('.main-items__button').forEach(button => {
+			button.classList.remove('main-items__button--active')
+		})
+		event.target.classList.add('main-items__button--active')
+	}
+})
+// Разворачиваем дескрипшен по клику
+const descriptionReadMoreButton = document.querySelector(
+	'.description-read-more__button'
+)
+const descriptionText = document.querySelector('.description-text')
+const descriptionSecondP = document.querySelector('.description__second-p')
+const descriptionThirdP = document.querySelector('.description__third-p')
+
+descriptionReadMoreButton.addEventListener('click', () => {
+	descriptionReadMoreButton.classList.toggle(
+		'description-read-more__button--active'
+	)
+	descriptionSecondP.classList.toggle('show')
+	descriptionThirdP.classList.toggle('show')
+
+	descriptionReadMoreButton.classList.contains(
+		'description-read-more__button--active'
+	)
+		? (descriptionReadMoreButton.textContent = 'Скрыть')
+		: (descriptionReadMoreButton.textContent = 'Читать далее')
+})
